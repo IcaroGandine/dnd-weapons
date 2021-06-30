c4c13e23efceea5e3eca422922b700f30dce1eaf
Icaro Weiler <icaro.weiler@codenapp.com>
2021-06-29 23:16:51 -0300

    create card component, import bootstrap as global

---
+++ src/app/weapon-card/weapon-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeaponCardComponent } from './weapon-card.component';
+
+describe('WeaponCardComponent', () => {
+  let component: WeaponCardComponent;
+  let fixture: ComponentFixture<WeaponCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WeaponCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WeaponCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/weapon-card/weapon-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weapon-card',
+  templateUrl: './weapon-card.component.html',
+  styleUrls: ['./weapon-card.component.scss']
+})
+export class WeaponCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
