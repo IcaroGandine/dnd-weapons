6b7406633fad22c9f24bf47cd215eb43a529720d
Icaro Weiler <icaro.weiler@codenapp.com>
2021-06-30 00:41:37 -0300

    create weapon list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeaponCardComponent } from './weapon-card/weapon-card.component';
+import { WeaponListComponent } from './weapon-list/weapon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeaponCardComponent
+    WeaponCardComponent,
+    WeaponListComponent
   ],
   imports: [
     BrowserModule,
